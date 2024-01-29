09f08e3207e73c444ef72f7b0d98b699e418780b
Karson Chrispens <karson.chrispens@ucsf.edu>
2024-01-28 21:32:12 -0800

    started tests, added packaging

--- cluster/__init__.py
+++ cluster/__init__.py
@@ -1,3 +1,9 @@
+"""
+BMI203: Biocomputing algorithms Winter 2024
+Assignment 3: K-Means Clustering
+"""
+__version__ = '1.0'
+
 from .kmeans import KMeans
 from .silhouette import Silhouette
 from .utils import (
--- test/test_kmeans.py
+++ test/test_kmeans.py
@@ -1,1 +1,16 @@
 # Write your k-means unit tests here
+import numpy as np
+import pytest
+from cluster import KMeans, make_clusters, plot_clusters, plot_multipanel
+
+def test_kmeans_invalid_inputs():
+    mat, labels = make_clusters()
+
+def test_kmeans_default():
+    mat, labels = make_clusters()
+
+    # Create KMeans instance and fit the data
+    kmeans = KMeans(k=2)
+    kmeans.fit(mat)
+
+    
